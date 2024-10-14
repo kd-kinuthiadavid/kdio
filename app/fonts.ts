1b9550c332004f9159903ab25cad203befe66ad0
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2024-10-14 12:13:23 +0300

    dev: initial draft

---
+++ app/fonts.ts
@@ -0,0 +1,11 @@
+import { Raleway, Karla } from "next/font/google";
+
+export const raleway = Raleway({
+  style: "normal",
+  subsets: ["latin"],
+});
+
+export const karla = Karla({
+  style: "normal",
+  subsets: ["latin"],
+});
