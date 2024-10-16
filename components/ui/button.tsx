6c8358606928431bd6d795a0e29028380e5e4746
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2024-10-16 12:22:57 +0300

    fix: update the text-color for outline btns

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -13,7 +13,7 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
-          "border border-primary bg-background shadow-sm hover:bg-foreground/5 hover:text-accent-foreground",
+          "border border-primary bg-background text-accent-foreground shadow-sm hover:bg-foreground/5 hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         accent: "bg-accent text-accent-foreground shadow-sm hover:bg-accent/80",
