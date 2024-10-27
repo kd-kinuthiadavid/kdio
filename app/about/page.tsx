5544c3ea2e29bab7530d3348278add1f626e9b54
kd-kinuthiadavid <kd.kinuthiadavid@gmail.com>
2024-10-27 12:34:29 +0300

    feat: add animations to the about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,44 +1,72 @@
-import { Button } from "@/components/ui/button";
+import * as motion from "framer-motion/client";
+
+import { containerVariants, itemVariants } from "../motionVariants";
+import AnimatedButton from "@/components/shared/AnimatedButton";
 
 export default function About() {
   return (
-    <div className="flex flex-col gap-y-4 max-w-[70%] 2xl:max-w-[50%]">
-      <h1 className="font-semibold text-5xl capitalize">About</h1>
-      <p className={`font-normal text-lg`}>
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      variants={containerVariants}
+      className="flex flex-col gap-y-4 max-w-[70%] 2xl:max-w-[50%]"
+    >
+      <motion.h1
+        variants={itemVariants}
+        className="font-semibold text-5xl capitalize"
+      >
+        About
+      </motion.h1>
+      <motion.p variants={itemVariants} className={`font-normal text-lg`}>
         I’m a product software engineer with a passion for building solutions
         that balance technical excellence and human-centered design. My unique
         value lies in my ability to:
-      </p>
-      <ul className="list-disc ml-8 text-lg space-y-2">
-        <li>
+      </motion.p>
+      <motion.ul
+        initial="hidden"
+        animate="visible"
+        variants={containerVariants}
+        className="list-disc ml-8 text-lg space-y-2"
+      >
+        <motion.li variants={itemVariants}>
           Translate complex technical challenges into easy-to-use, user-driven
           solutions.
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={itemVariants}>
           Ensure consistency between the product vision and the final user
           experience, from backend architecture to front-end design.
-        </li>
-        <li>
+        </motion.li>
+        <motion.li variants={itemVariants}>
           Collaborate effectively with product managers, designers, and
           cross-functional teams to deliver impactful, scalable products.
-        </li>
-      </ul>
-      <p className={`font-normal text-lg`}>
+        </motion.li>
+      </motion.ul>
+      <motion.p variants={itemVariants} className={`font-normal text-lg`}>
         I’m here to help you bridge the gap between engineering and user
         experience, ensuring your product doesn’t just work—but works
         beautifully for your users.
-      </p>
-      <div className="flex gap-x-5">
-        <Button className="w-full text-base font-medium capitalize py-6">
+      </motion.p>
+      <motion.div
+        initial="hidden"
+        animate="visible"
+        variants={containerVariants}
+        className="flex gap-x-5"
+      >
+        <AnimatedButton
+          motionVariants={itemVariants}
+          variant={"default"}
+          className="w-full text-base font-medium capitalize py-6"
+        >
           Let&apos;s build together
-        </Button>
-        <Button
+        </AnimatedButton>
+        <AnimatedButton
+          motionVariants={itemVariants}
           variant={"outline"}
           className="w-full text-base font-medium capitalize py-6"
         >
           Explore my method
-        </Button>
-      </div>
-    </div>
+        </AnimatedButton>
+      </motion.div>
+    </motion.div>
   );
 }
