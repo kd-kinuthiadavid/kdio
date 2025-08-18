46629a75c5332d6f862d5e13b511b4b5c24f931b
kd_kinuthiadavid <kd.kinuthiadavid@gmail.com>
2025-08-18 16:32:50 +0300

    feat: add improvs to the 'about' page layout
    update the layout to use accordion sections & chips

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,17 +2,44 @@
 import { motion } from "framer-motion";
 import { useRouter } from "next/navigation";
 
-import { containerVariants, itemVariants } from "../motionVariants";
+import {
+  containerVariants,
+  itemVariants,
+  navItemVariants,
+} from "../motionVariants";
 import AnimatedButton from "@/components/shared/AnimatedButton";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionTrigger,
+  AccordionContent,
+} from "@/components/ui/accordion";
+import { Badge } from "@/components/ui/badge";
 
 export default function About() {
   const router = useRouter();
+
+  const MotionAccordionItem = motion.create(AccordionItem);
+  const MotionAccordionTrigger = motion.create(AccordionTrigger);
+  const MotionAccordionContent = motion.create(AccordionContent);
+  const MotionBadge = motion.create(Badge);
+
+  const expertiseList = [
+    "Product & User Experience",
+    "AI & Intelligent Systems",
+    "UI.UX & Frontend Development",
+    "Backend Development & APIs",
+    "Systems Architecture & Scalability",
+    "Product development & strategy",
+    "agile & cross-functional leadership",
+  ];
+
   return (
     <motion.div
       initial="hidden"
       animate="visible"
       variants={containerVariants}
-      className="flex flex-col gap-y-4 md:max-w-[70%] 2xl:max-w-[50%]"
+      className="flex flex-col gap-y-4 w-full md:max-w-[50%]"
     >
       <motion.h1
         variants={itemVariants}
@@ -20,41 +47,115 @@ export default function About() {
       >
         About
       </motion.h1>
-      <motion.p
-        variants={itemVariants}
-        className={`font-normal text-base md:text-lg`}
-      >
-        I’m a product software engineer with a passion for building solutions
-        that balance technical excellence and human-centered design. My unique
-        value lies in my ability to:
-      </motion.p>
-      <motion.ul
+      <Accordion
         initial="hidden"
         animate="visible"
         variants={containerVariants}
-        className="list-disc ml-8 text-base md:text-lg space-y-2"
+        type="single"
+        collapsible
+        className="grid gap-6 grid-cols-1 w-full"
       >
-        <motion.li variants={itemVariants}>
-          Translate complex technical challenges into easy-to-use, user-driven
-          solutions.
-        </motion.li>
-        <motion.li variants={itemVariants}>
-          Ensure consistency between the product vision and the final user
-          experience, from backend architecture to front-end design.
-        </motion.li>
-        <motion.li variants={itemVariants}>
-          Collaborate effectively with product managers, designers, and
-          cross-functional teams to deliver impactful, scalable products.
-        </motion.li>
-      </motion.ul>
-      <motion.p
-        variants={itemVariants}
-        className={`font-normal text-base md:text-lg`}
-      >
-        I’m here to help you bridge the gap between engineering and user
-        experience, ensuring your product doesn’t just work—but works
-        beautifully for your users.
-      </motion.p>
+        {/* who I am */}
+        <MotionAccordionItem
+          variants={navItemVariants}
+          value={`about-david-kinuthia:who-i-am`}
+          className={`px-4 border-b border-black/40`}
+        >
+          <MotionAccordionTrigger
+            initial="hidden"
+            animate="visible"
+            variants={containerVariants}
+            className="flex items-start group data-[state=open]:visible"
+          >
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={containerVariants}
+              className="flex items-end gap-x-5"
+            >
+              <motion.p
+                variants={itemVariants}
+                className="text-base md:text-xl text-left font-normal capitalize text-accent-foreground"
+              >
+                01
+              </motion.p>
+              <motion.p
+                variants={itemVariants}
+                className="text-base md:text-lg text-left font-semibold capitalize"
+              >
+                Who I am
+              </motion.p>
+            </motion.div>
+          </MotionAccordionTrigger>
+
+          {/* content */}
+          <MotionAccordionContent
+            initial="hidden"
+            animate="visible"
+            variants={containerVariants}
+          >
+            <motion.p variants={itemVariants} className="text-lg mb-2">
+              I’m a Product Software Engineer with 7+ years of full-stack
+              experience, evolving from individual contributor to technical
+              leader, driven by a passion for crafting elegant, scalable, and
+              reliable systems that delight users
+            </motion.p>
+          </MotionAccordionContent>
+        </MotionAccordionItem>
+
+        {/* what I do */}
+        <MotionAccordionItem
+          variants={navItemVariants}
+          value={`about-david-kinuthia:what-i-do`}
+          className={`px-4 border-b border-black/40`}
+        >
+          <MotionAccordionTrigger
+            initial="hidden"
+            animate="visible"
+            variants={containerVariants}
+            className="flex items-start group data-[state=open]:visible"
+          >
+            <motion.div
+              initial="hidden"
+              animate="visible"
+              variants={containerVariants}
+              className="flex items-end gap-x-5"
+            >
+              <motion.p
+                variants={itemVariants}
+                className="text-base md:text-xl text-left font-normal capitalize text-accent-foreground"
+              >
+                02
+              </motion.p>
+              <motion.p
+                variants={itemVariants}
+                className="text-base md:text-lg text-left font-semibold capitalize"
+              >
+                What I do
+              </motion.p>
+            </motion.div>
+          </MotionAccordionTrigger>
+
+          {/* content */}
+          <MotionAccordionContent
+            initial="hidden"
+            animate="visible"
+            variants={containerVariants}
+            className="flex flex-wrap gap-1.5"
+          >
+            {expertiseList.map((expertise, idx) => (
+              <MotionBadge
+                variants={navItemVariants}
+                variant={"outline"}
+                key={idx}
+                className="rounded-2xl font-medium text-base capitalize"
+              >
+                {expertise}
+              </MotionBadge>
+            ))}
+          </MotionAccordionContent>
+        </MotionAccordionItem>
+      </Accordion>
       <motion.div
         initial="hidden"
         animate="visible"
