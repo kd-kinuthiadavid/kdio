96ba275d499ee9e3b4735f489f78c039e37a82ce
kd_kinuthiadavid <kd.kinuthiadavid@gmail.com>
2026-05-12 18:32:32 +0300

    fix: update hero section

--- app/page.tsx
+++ app/page.tsx
@@ -13,54 +13,54 @@ import { useRouter } from "next/navigation";
 export default function Home() {
   const router = useRouter();
   return (
-    <ContentSurface className="w-full md:max-w-[60%] 2xl:max-w-[40%]">
+    <ContentSurface className="w-full md:max-w-[80%] 2xl:max-w-[60%]">
       <motion.div
         initial="hidden"
         animate="visible"
         variants={containerVariants}
         className="flex flex-col gap-y-6"
       >
-      <motion.h1
-        variants={itemVariants}
-        className="font-semibold text-4xl md:text-5xl capitalize"
-      >
-        Crafting Scalable Solutions Where Engineering Meets Empathy.
-      </motion.h1>
-      <motion.p
-        variants={navItemVariants}
-        className={`font-normal text-lg md:text-xl md:max-w-[95%]`}
-      >
-        Hello, my name is David Kinuthia. I am a product software engineer based
-        in Nairobi. I blend technical expertise and user empathy to build
-        products that are reliable, intuitive, and meaningful— delivering
-        exceptional experiences from backend to interface.
-      </motion.p>
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        variants={containerVariants}
-        className="flex flex-wrap md:flex-nowrap gap-x-5 gap-y-2.5"
-      >
-        <AnimatedButton
-          motionVariants={navItemVariants}
-          variant={"default"}
-          className="w-full text-base font-medium capitalize py-6"
-          onClick={() =>
-            window.open("https://cal.com/kinuthiadavid/15min", "_blank")
-          }
+        <motion.h1
+          variants={itemVariants}
+          className="font-semibold text-5xl md:text-7xl capitalize"
         >
-          Book a 15-min Intro
-        </AnimatedButton>
-        <AnimatedButton
-          motionVariants={navItemVariants}
-          variant={"outline"}
-          className="w-full text-base font-medium capitalize py-6"
-          onClick={() => router.push("/work")}
+          Crafting Scalable Solutions Where Engineering Meets Empathy.
+        </motion.h1>
+        <motion.p
+          variants={navItemVariants}
+          className={`font-normal text-lg md:text-xl md:max-w-[95%]`}
         >
-          see selected work
-        </AnimatedButton>
+          Hello, my name is David Kinuthia. I am a product software engineer
+          based in Nairobi. I blend technical expertise and user empathy to
+          build products that are reliable, intuitive, and meaningful—
+          delivering exceptional experiences from backend to interface.
+        </motion.p>
+        <motion.div
+          initial="hidden"
+          animate="visible"
+          variants={containerVariants}
+          className="flex flex-wrap md:flex-nowrap gap-x-5 gap-y-2.5"
+        >
+          <AnimatedButton
+            motionVariants={navItemVariants}
+            variant={"default"}
+            className="w-full text-base font-medium capitalize py-6"
+            onClick={() =>
+              window.open("https://cal.com/kinuthiadavid/15min", "_blank")
+            }
+          >
+            Book a 15-min Intro
+          </AnimatedButton>
+          <AnimatedButton
+            motionVariants={navItemVariants}
+            variant={"outline"}
+            className="w-full text-base font-medium capitalize py-6"
+            onClick={() => router.push("/work")}
+          >
+            see selected work
+          </AnimatedButton>
+        </motion.div>
       </motion.div>
-    </motion.div>
     </ContentSurface>
   );
 }
